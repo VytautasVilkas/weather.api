f9be5d046911da67c3a97d77a75dd887e7dc13bc
Vytautas Vilkas <126881069+VytautasVilkas@users.noreply.github.com>
2024-09-04 21:39:40 +0300

    fixing issues

--- App.js
+++ App.js
@@ -3,15 +3,26 @@ const cors = require('cors');
 const { getWeather } = require('./controllers/weatherController'); 
 const app = express();
 
+// List of allowed origins (local and deployed frontend)
+const allowedOrigins = [
+  'http://localhost:3000',  // For local development
+  'https://vytautasvilkas.vercel.app',  // Deployed frontend
+];
+
+// CORS configuration
 app.use(cors({
-  origin: 'https://vytautasvilkas.vercel.app',  
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);   (e.g., Postman)
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,  
 }));
 app.use(express.json());
 app.get('/api/weather', getWeather);  
-
+const port = process.env.PORT || 8080;  
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
